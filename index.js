633cedf3649f8c5ace0988ae2355e48596b6a2b5
evo264714 <mh264714@gmail.com>
2023-06-12 05:33:28 +0600

    done making my classes apis and student dashboard

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT ||5000
+const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors())
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmw7zrv.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,14 +27,53 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("summerDb").collection("users");
     const instructorCollection = client.db("summerDb").collection("instructor");
+    const classCollection = client.db("summerDb").collection("class")
+    const singleUserClassCollection = client.db("summerDb").collection("singleUserClass")
 
-    app.get('/instructor', async(req, res) =>{
-        const result = await instructorCollection.find().toArray();
-        res.send(result)
+    //Users related Apis
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     })
 
 
+    //Instructors related Apis
+    app.get('/instructor', async (req, res) => {
+      const result = await instructorCollection.find().toArray();
+      res.send(result)
+    })
+
+    //Classes collection apis
+    app.get('/classes', async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result)
+    })
+
+    //Single user class collection apis
+    app.post('/singleuserclass', async (req, res) => {
+      const item = req.body;
+      const result = await singleUserClassCollection.insertOne(item)
+      res.send(result)
+    })
+
+    app.get('/singleuserclass', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([])
+      }
+      const query = { email: email }; const result = await singleUserClassCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.delete('/singleuserclass/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await singleUserClassCollection.deleteOne(query)
+      res.send(result);
+    })
 
 
 
@@ -50,10 +89,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send('Camp is here')
+app.get('/', (req, res) => {
+  res.send('Camp is here')
 })
 
-app.listen(port, () =>{
-    console.log(`Camp is here on port ${port}`);
+app.listen(port, () => {
+  console.log(`Camp is here on port ${port}`);
 })
